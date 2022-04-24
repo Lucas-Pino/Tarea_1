d426969e23a53e722254ef0ee00c8691132437af
Lucas Pino <lucaspinojara@gmail.com>
2022-04-24 17:35:06 -0400

    nico hizo cositas

---
+++ src/Stage3/RollerShade.java
@@ -0,0 +1,79 @@
+public class RollerShade extends DomoticDevice {
+    public RollerShade (int channel, double alpha, double length) {
+        this.motor = motor;
+        this.MaxShadeLength = length;
+        this.length = length;
+        motor = new Motor(alpha, channel);
+    }
+    public Motor.MotorState getMotorState(){
+        return motor.state;
+    }
+    public void startUp(){
+        motor.turnUp();
+    }
+    public void startDown(){
+        motor.turnDown();
+    }
+    public void stop(){
+        motor.stop();
+    }
+    public void advanceTime(double delta){
+        motor.advanceTime(delta);
+    }
+    public String getHeader(){
+        String s = "RS" + getId();
+        return s;
+    }
+    public String toString(){
+        String s = String.valueOf(Math.round(length/MaxShadeLength*100));
+        return s;
+    }
+    private class Motor {  //nested class, Motor is only used within RollerShade.
+        public Motor (double a, int ch){
+            channel = ch;
+            alpha = a;
+            state = MotorState.STOPPED;
+        }
+        public MotorState getMotorState(){
+            return state;
+        }
+        public enum MotorState {
+            UPWARD,
+            STOPPED,
+            DOWNWARD
+        }
+        public void turnUp(){
+            state = MotorState.UPWARD;
+        }
+        public void turnDown(){
+            state = MotorState.DOWNWARD;
+        }
+        public void stop(){
+            state = MotorState.STOPPED;
+        }
+        public void advanceTime(double delta){
+
+            double increment = alpha*delta*RADIUS;
+            switch (state) {
+                case STOPPED: break;
+                case DOWNWARD:
+                    length =+ increment;
+                    break;
+                case UPWARD:
+                    length =- increment;
+                    break;
+            }
+        }
+        private double alpha;
+        private int channel;
+        private MotorState state;
+        private static double RADIUS=0.04;
+    }
+
+    private Motor motor;
+    private double length;
+    private double MaxShadeLength;
+    private static int nextId;
+
+
+}
